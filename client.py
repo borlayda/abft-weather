a99dc805eec04d49f7e3a3b0cad4123d70d50010
Borlay Dániel <borlay.daniel@gmail.com>
2015-12-10 00:20:15 +0100

    Error handling

--- client.py
+++ client.py
@@ -6,13 +6,17 @@ from matrix import *
 HOST_URL = "https://totlol.me/temp"
 
 def get_weekly_data():
-    data = get(HOST_URL)
-    matrices = data.json()["matrices"]
-    checksums = data.json()["sum"]
-    summ_matrix, c_check, r_check = make_summ(matrices)
-    print_matrix(summ_matrix)
-    iterate_matrix(checksums, r_check, c_check)
-    print_matrix(checksums)
+    try:
+        data = get(HOST_URL)
+        matrices = data.json()["matrices"]
+        checksums = data.json()["sum"]
+        summ_matrix, c_check, r_check = make_summ(matrices)
+        print_matrix(summ_matrix)
+        iterate_matrix(checksums, r_check, c_check)
+        print_matrix(checksums)
+    except Exception as e:
+        print "Cannot connect to host: {0}, {1}".format(
+            HOST_URL, e)
 
 if __name__ == '__main__':
     get_weekly_data()
